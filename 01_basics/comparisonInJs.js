49ea87caff0f37948e5a76977d55de245e208ebe
Mohd Nawaz <94606718+zaifee@users.noreply.github.com>
2024-02-17 19:27:48 +0000

    adding comparison

---
+++ 01_basics/comparisonInJs.js
@@ -0,0 +1,25 @@
+console.log(2 > 1); 
+console.log( 2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log( 2 != 1);
+
+//all the above statement are simple either they give result true or false 
+
+//yaha faste hai 
+console.log("Printing Some complex stuff");
+console.log("2" > 1);  //true
+console.log("02" > 1);  //true
+console.log("Null Result : ");
+console.log(null > 0); 
+console.log( null == 0);
+console.log(null >= 0); //true 
+
+
+//Some complex stuff
+console.log("printing undefined results: ");
+console.log(undefined == 0); //false 
+console.log(undefined > 0); //false
+console.log(undefined <= 0); //false
+
+//we have to avoid such confusions in code 
