e060019f741dc50c7bec652e78991f347da51b64
Mohd Nawaz <94606718+zaifee@users.noreply.github.com>
2024-02-17 17:53:17 +0000

    adding basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 12234;
+let accountEmail = "hello@gmail.com";
+var accountPassword = "12345";
+accountCity = "Jaipur"
+
+
+
+accountEmail = "hello@mail.com";
+accountPassword = 2367;
+accountCity = "Deoband";
+console.log(accountEmail);
+console.log(accountPassword);
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+
+/*
+Prefer not to use because of block scope and funtional scope
+*/
