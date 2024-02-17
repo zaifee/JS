2b47e7f402fb484a333af765ca489443dc312d22
Mohd Nawaz <94606718+zaifee@users.noreply.github.com>
2024-02-17 19:15:12 +0000

    adding operators

---
+++ 01_basics/operators.js
@@ -0,0 +1,11 @@
+//Arithmetic operator +,-,*,/,%, **(power);
+
+//concatenating the string 
+let str1 = "good";
+let str2 = " Morning";
+console.log(str1 + str2);
+
+
+//Problem occured here 
+console.log("1" + 2);
+console.log(1 + "2");
