f94f2a84f45fc6240f0395720320523271630cc2
Mohd Nawaz <94606718+zaifee@users.noreply.github.com>
2024-03-29 10:51:59 +0000

    adding forOf loops

---
+++ 03_basics/loop.js
@@ -0,0 +1,11 @@
+const arr = [1, 2,3, 4, 5,6];
+for (const iterator of arr) {
+    console.log(iterator);
+}
+
+//it can be applied on the strings as well 
+const greeting = "hello ji";
+for (const greet of greeting) {
+    console.log(`Each char is ${greet}`);
+}
+
