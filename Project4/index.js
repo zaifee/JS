ab87bada1fb43a70a05a697b17294dc76ca220a4
Mohd Nawaz <94606718+zaifee@users.noreply.github.com>
2024-06-21 12:20:37 -0700

    Create index.js

---
+++ Project4/index.js
@@ -0,0 +1,82 @@
+const randomNo = Math.round(Math.random() * 10 + 1);
+
+const submit = document.querySelector('#subt');
+const userInput = document.querySelector('#guessField');
+
+const guessSlot = document.querySelector('.guesses');
+const remRes = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+const startOver = document.querySelector('.resultParas');
+
+
+const p = document.createElement('p');
+
+let prevGuess = [];
+let numGuess = 1;
+
+let playGame = true;
+
+if(playGame){
+  submit.addEventListener('click', function(e){
+    e.preventDefault();
+    const guess = parseInt(userInput.value);
+    validateGuess(guess);
+  });
+}
+
+function validateGuess(guess){
+if(isNan(guess)){
+  alert('Please enter a valid Number');
+
+}else if(guess < 1){
+  alert('Please enter a number more than 1');
+}else if(guess > 100){
+  alert('Please enter a number that lie in the range between 1 and 100');
+}else{
+  prevGuess.push(guess);
+  if(numGuess === 11){
+    displayGuess(guess);
+    displayMessage(`Game Over. Random Number was ${randomNo}`);
+    endGame();
+    
+  }else{
+    displayGuess(guess);
+    checkGuess(guess);
+  }
+}
+
+}
+
+function checkGuess(guess){
+ if(guess === randomNo){
+   displayMessage(`You guesses it right `);
+   endGame();
+ }else if(guess < randomNo){
+   displayMessage(`Number is too Low`);
+ }else if(guess > randomNo){
+   displayMessage(`Number is too high`);
+ }
+
+}
+
+function displayGuess(){
+  userInput.value = '';
+  guessSlot.innerHTML += 
+
+}
+
+function displayMessage(message){
+  //all the dom Manipulation comes here 
+
+
+}
+
+
+function startOver(){
+
+}
+
+function endGame(){
+
+
+}
