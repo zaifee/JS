35661533767d3e823052e50d59aea0beae668021
Mohd Nawaz <94606718+zaifee@users.noreply.github.com>
2024-06-27 01:24:21 -0700

    Update index.js

--- Project4/index.js
+++ Project4/index.js
@@ -8,7 +8,6 @@ const remRes = document.querySelector('.lastResult');
 const lowOrHi = document.querySelector('.lowOrHi');
 const startOver = document.querySelector('.resultParas');
 
-
 const p = document.createElement('p');
 
 let prevGuess = [];
@@ -16,67 +15,58 @@ let numGuess = 1;
 
 let playGame = true;
 
-if(playGame){
-  submit.addEventListener('click', function(e){
+if (playGame) {
+  submit.addEventListener('click', function (e) {
     e.preventDefault();
     const guess = parseInt(userInput.value);
     validateGuess(guess);
   });
 }
 
-function validateGuess(guess){
-if(isNan(guess)){
-  alert('Please enter a valid Number');
-
-}else if(guess < 1){
-  alert('Please enter a number more than 1');
-}else if(guess > 100){
-  alert('Please enter a number that lie in the range between 1 and 100');
-}else{
-  prevGuess.push(guess);
-  if(numGuess === 11){
-    displayGuess(guess);
-    displayMessage(`Game Over. Random Number was ${randomNo}`);
-    endGame();
-    
-  }else{
-    displayGuess(guess);
-    checkGuess(guess);
+function validateGuess(guess) {
+  if (isNaN(guess)) {
+    alert('Please enter a valid Number');
+  } else if (guess < 1) {
+    alert('Please enter a number more than 1');
+  } else if (guess > 100) {
+    alert('Please enter a number that lie in the range between 1 and 100');
+  } else {
+    prevGuess.push(guess);
+    if (numGuess === 11) {
+      displayGuess(guess);
+      displayMessage(`Game Over. Random Number was ${randomNo}`);
+      endGame();
+    } else {
+      displayGuess(guess);
+      checkGuess(guess);
+    }
   }
 }
 
+function checkGuess(guess) {
+  if (guess === randomNo) {
+    displayMessage(`You guesses it right `);
+    endGame();
+  } else if (guess < randomNo) {
+    displayMessage(`Number is too Low`);
+  } else if (guess > randomNo) {
+    displayMessage(`Number is too high`);
+  }
 }
 
-function checkGuess(guess){
- if(guess === randomNo){
-   displayMessage(`You guesses it right `);
-   endGame();
- }else if(guess < randomNo){
-   displayMessage(`Number is too Low`);
- }else if(guess > randomNo){
-   displayMessage(`Number is too high`);
- }
-
-}
-
-function displayGuess(){
+function displayGuess() {
   userInput.value = '';
-  guessSlot.innerHTML += 
-
+  guessSlot.innerHTML += `${guess}`;
+  numGuess++;
+  remRes.innerHTML = `${11 - numGuess}`;
 }
 
-function displayMessage(message){
-  //all the dom Manipulation comes here 
+function displayMessage(message) {
+  //all the dom Manipulation comes here
 
-
+  lowOrHi.innerHTML = `<h2>${message}</h2>`;
 }
 
+function newGame() {}
 
-function startOver(){
-
-}
-
-function endGame(){
-
-
-}
+function endGame() {}
